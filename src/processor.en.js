769d1e8b7437ddb2fe20beda73d09c847f94e0de
Daniel <daniel@MacBook-Pro.local>
2019-04-12 07:49:54 +0800

    wip: article split

--- src/processor.en.js
+++ src/processor.en.js
@@ -28,8 +28,11 @@ exports.processor = (article) => {
 
 /*
 
-
-function parseArticle(article) {
+function splitArticles(article) {
+  const results = [];
+  const copy = Object.assign({}, article);
+  delete copy.text;
+  
   const lines =
     article.text.replace(/\r\n/g, "\r").replace(/\n/g, "\r").split(/\r/);
 
@@ -37,58 +40,4 @@ function parseArticle(article) {
   const reHeading = /(=+).*\1/;
 
 }
-
-// let count = 0;
-// let categories = [];
-// let headings = [];
-const reHeading = /(=+).*\1/;
-
-function parsePage(page) {
-  const pageContent = page.revision.text['$text'];
-  const pageTitle = page.title['$text'];
-
-  if ((pageTitle === null) || (pageTitle === undefined)) {
-    // console.log(`Error: got a page with no title: ${page}`);
-  }
-
-  if (pageTitle.indexOf(':') !== -1) {
-    // let category = pageTitle.slice(0, pageTitle.indexOf(':'));
-    // if (!categories.includes(category)) categories.push(category);
-  } else {
-    console.log(pageTitle);
-    if ((pageContent === null) || (pageContent === undefined)) {
-      // console.log(`Error: "${pageTitle}" has no content`);
-    } else {
-
-      // console.log(page.revision.text);
-
-      // const lines = pageContent.split(/\r?\n/);
-      const lines =
-        pageContent.replace(/\r\n/g, "\r").replace(/\n/g, "\r").split(/\r/);
-
-      // console.log(pageTitle + '(' + lines.length + ')');
-      // console.log('\n\n');
-      // console.log(pageContent);
-      // console.log('\n\n\n\n');
-
-      for (let i = 0; i < lines.length -1; i++) {
-        let line = lines[i].trim();
-        let h = line.match(reHeading);
-        if (h !== null) {
-          h = h[0];
-          if (!headings.includes(h)) headings.push(h);
-        }
-      }
-    }
-  }
-
-  count++;
-  // if (count > 10000) xParser.pause();
-}
 */
-
-// Etymology
-// Pronunciation
-// ===Noun===
-// ====Synonyms====
-// ====Derived terms====
